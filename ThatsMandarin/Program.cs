25c81fe5811494a0ab33d79fd334c53abe3c645f
adsieg01 <adsieg01@gmail.com>
2018-01-25 16:25:10 -0500

    Wrote thoughts about the first challenge

--- ThatsMandarin/Program.cs
+++ ThatsMandarin/Program.cs
@@ -5,13 +5,19 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
+
 namespace ThatsMandarin
 {
     class Program
     {
         public static void Main(string[] args)
         {
-           
+            //Probelem 1
+           //Almost solved this challenege but I cannot get the occurance of words to print
+           //I am only able to get characters to print
+           //The problem is this sentence has English and Chinese but how words are made differ
+           //Chinese words consist of one or two characters and English words have many characters
+           //This is a Chinese sentence which has no spacing between words
             string str = "你好\r\n我叫John，\r\n我是美国人。\r\n我喜欢中国。";                   
             str = Regex.Replace(str, @"[^\w]", string.Empty);
             var result = str.ToArray()
