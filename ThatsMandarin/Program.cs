f40e86b9e0d90d6e9da315b3c4f36e8e964f4ce7
adsieg01 <adsieg01@gmail.com>
2018-01-25 15:10:32 -0500

    Partially solved first question

--- ThatsMandarin/Program.cs
+++ ThatsMandarin/Program.cs
@@ -2,14 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ThatsMandarin
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+           
+            string str = "你好\r\n我叫John，\r\n我是美国人。\r\n我喜欢中国。";                   
+            str = Regex.Replace(str, @"[^\w]", string.Empty);
+            var result = str.ToArray()
+                            .GroupBy(word => word)
+                            .Select(grp => new
+                            {
+                                Word = grp.Key,
+                                Count = grp.Count()
+                            });
+
+            foreach (var item in result)
+            {
+                
+                Console.WriteLine("Word: {0} Count:{1}", item.Word, item.Count);
+            }
+
+            Console.ReadLine();
+
+
+
+
         }
     }
+    
 }
